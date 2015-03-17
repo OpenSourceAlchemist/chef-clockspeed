e1ab3c144adc528cd0789c36d4d4a06c2c500365
Kevin Berry <kevin@opensourcealchemist.com>
2015-03-16 20:01:23 -0500

    Config file (clockspeed.conf) and some more attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,8 @@ default[:clockspeed][:wait] = 541 # Initial wait interval for adjustment mark (i
 default[:clockspeed][:wait_max] = 2617923 # Maximum time to wait for adjustment mark (in seconds)
 default[:clockspeed][:user] = 'clocksd' # User to run services as (reduce privileges for safety)
 # A list of NTP servers to use, sourced from `dig us.pool.ntp.org +short`
-default[:clockspeed][:ntp_servers] = %w{66.228.35.252 184.105.182.7 108.61.73.244 208.43.245.212} 
+default[:clockspeed][:ntp_servers] = %w(66.228.35.252 184.105.182.7 108.61.73.244 208.43.245.212) 
+default[:clockspeed][:clock_type] = 'ntp'
 
 #Toggles for each service (if for some reason you need to disable some of this)
 default[:clockspeed][:clockspeed_enabled] = true
@@ -14,6 +15,8 @@ default[:clockspeed][:sv_base] = '/etc/sv' # Where to define our services
 default[:clockspeed][:service_base] = '/etc/service' # Where link our services to enable
 default[:clockspeed][:prefix] = '/usr/local'
 default[:clockspeed][:djb_base] = File.join(node[:clockspeed][:prefix],"djb")
+default[:clockspeed][:base] = File.join(node[:clockspeed][:prefix],'clockspeed')
+default[:clockspeed][:bin_dir] = File.join(node[:clockspeed][:base],'bin')
 
 # Source details
 default[:clockspeed][:source_md5sum] = '425614174fcfe2ad42d22d3d02e2d567'
